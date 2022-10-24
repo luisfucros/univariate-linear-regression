04e37b7425859f5cfde6185d8cceee1a0061bda6
luisfucros <luisfucros@gmail.com>
2022-10-23 19:09:10 -0500

    first comment test

--- tests/models/test_train.py
+++ tests/models/test_train.py
@@ -25,6 +25,7 @@ class TestSplitIntoTrainingAndTestingSets(object):
 
     def test_on_one_row(self):
         test_argument = np.array([[1382.0, 390167.0]])
+        # testing for exceptions, raises an error if the function runs with no errors
         with pytest.raises(ValueError) as exc_info:
             split_into_training_and_testing_sets(test_argument)
         expected_error_msg = "Argument data_array must have at least 2 rows, it actually has just 1"
